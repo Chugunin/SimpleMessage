8632e8594f3fb97e654d0b804759248290f973be
Lelouch <andreychugunin@gmail.com>
2021-08-03 23:01:42 +0300

    Initial commit

---
+++ src/main/java/com/example/simplemessage/SimpleMessageApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplemessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMessageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleMessageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simplemessage/SimpleMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplemessage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleMessageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
